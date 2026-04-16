741db89ddbc8d4db297af390fbd6d7527a7a929f
Naseef Sharaf <nashafeng32@gmail.com>
2026-04-16 21:49:32 +0530

    Update on results

--- bmi_calc.js
+++ bmi_calc.js
@@ -14,10 +14,27 @@ function calculateBMI() {
     bmi = parseFloat(bmi.toFixed(2));
 
     document.getElementById("result").innerHTML = "Your BMI is: " + bmi;
+
+    if (bmi < 18.5) {
+        document.getElementById("bmi-result").innerHTML = "Under Weight";
+    }
+    else if (bmi >= 18.5 && bmi <= 24.9) {
+        document.getElementById("bmi-result").innerHTML = "Healthy Weight";
+    }
+
+    else if (bmi >= 25.0 && bmi <= 29.9) {
+        document.getElementById("bmi-result").innerHTML = "Over Weight";
+    }
+
+    else {
+        document.getElementById("bmi-result").innerHTML = "Obesity";
+    }
+
 }
 
 function resetFields() {
     document.getElementById("weight").value = "";
     document.getElementById("height").value = "";
     document.getElementById("result").innerHTML = "";
+    document.getElementById("bmi-result").innerHTML = "";
 }
